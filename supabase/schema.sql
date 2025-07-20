f2da1cc481e29277bbe74201dc54c15a9ea2b7d6
terenkur <hillsboro96@gmail.com>
2025-07-21 01:01:21 +0300

    Replace nickname columns with username and user_id

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -5,7 +5,7 @@ create table if not exists items (
 
 create table if not exists users (
   id serial primary key,
-  nickname text
+  username text
 );
 
 create table if not exists games (
@@ -22,10 +22,10 @@ create table if not exists votes (
   id serial primary key,
   poll_id integer references polls(id),
   game_id integer references games(id),
-  voter_nickname integer references users(id)
+  user_id integer references users(id)
 );
 
-create index if not exists votes_voter_nickname_idx on votes(voter_nickname);
+create index if not exists votes_user_id_idx on votes(user_id);
 
 create index if not exists votes_poll_id_idx on votes(poll_id);
 create index if not exists votes_game_id_idx on votes(game_id);
