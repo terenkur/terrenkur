8d49ca0686ae4eafdddc4777e591de4a69b4c50f
terenkur <hillsboro96@gmail.com>
2025-08-14 12:33:19 +0300

    feat: add card variants

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+export interface CardProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  variant?: "plain" | "shadow";
+}
 
 const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ className, ...props }, ref) => (
+  ({ className, variant = "shadow", ...props }, ref) => (
     <div
       ref={ref}
       className={cn(
-        "rounded-lg bg-muted p-4 border",
+        "rounded-lg bg-muted p-4 border shadow-sm",
+        variant === "plain" && "border-none shadow-none",
         className
       )}
       {...props}
