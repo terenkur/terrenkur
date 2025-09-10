c0305a1a43f2a3e1c8dbe8d28b49997d498a4f73
terenkur <hillsboro96@gmail.com>
2025-09-10 20:29:12 +0300

    add centered transparent obs layout

---
+++ frontend/app/obs/layout.tsx
@@ -0,0 +1,12 @@
+import '../globals.css';
+import type { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body className="flex items-center justify-center min-h-screen bg-transparent pointer-events-none">
+        {children}
+      </body>
+    </html>
+  );
+}
